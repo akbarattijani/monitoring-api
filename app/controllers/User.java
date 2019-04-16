1ece3ca8610827aff3168857b69b30956f1c2475
Akbar <akbar.attijani@gmail.com>
2019-04-16 11:41:00 +0700

    Modify User API

--- app/controllers/User.java
+++ app/controllers/User.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import database.Connection.CheckDatabase;
 import database.Connection.Connection;
@@ -38,16 +39,43 @@ public class User {
             ObjectMapper mapper = new ObjectMapper();
             final UserModel body = mapper.treeToValue(request().body().asJson(), UserModel.class);
 
-            String sql = "INSERT INTO m_user (nip, name, password) values (?,?,?)";
+            String sql = "INSERT INTO m_user (nip, name, password, step) values (?,?,?,?)";
             PreparedStatement preparedStatement = Connection.getConnection().prepareStatement(sql);
 
             preparedStatement.setString(1, body.getNip());
             preparedStatement.setString(2, body.getName());
             preparedStatement.setString(3, body.getPassword());
+            preparedStatement.setInt(4, 1);
 
             int count = preparedStatement.executeUpdate();
 
-            return Body.echo(enums.Result.REQUEST_OK, "Inserting Success : " + count);
+            if (count > 0) {
+                JSONObject object = new JSONObject();
+                String select = "SELECT * FROM m_user WHERE nip = ? limit 1";
+
+                PreparedStatement prepaired = Connection.getConnection().prepareStatement(select);
+                prepaired.setString(1, body.getNip());
+
+                ResultSet rs = prepaired.executeQuery();
+                if (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nipResult = rs.getString("nip");
+                    String name = rs.getString("name");
+                    int step = rs.getInt("step");
+
+                    object.put("status", 1);
+                    object.put("id", id);
+                    object.put("nip", nipResult);
+                    object.put("name", name);
+                    object.put("step", step);
+                } else {
+                    object.put("status", 0);
+                }
+
+                return Body.echo(enums.Result.REQUEST_OK, object.toString());
+            }
+
+            return Body.echo(enums.Result.RESPONSE_NOTHING, "Inserting Failed...");
         } catch (Exception e) {
             e.printStackTrace();
             return status(401, e.getMessage());
@@ -76,15 +104,82 @@ public class User {
                 int id = rs.getInt("id");
                 String nipResult = rs.getString("nip");
                 String name = rs.getString("name");
+                int step = rs.getInt("step");
 
                 object.put("id", id);
                 object.put("nip", nipResult);
                 object.put("name", name);
+                object.put("step", step);
 
                 return Body.echo(enums.Result.REQUEST_OK, object.toString());
             }
 
             return Body.echo(enums.Result.RESPONSE_NOTHING, "User Not Found");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return status(401, e.getMessage());
+        }
+    }
+
+    @CheckDatabase(
+            database = Database.POSTGRESQL,
+            host = "ec2-23-23-173-30.compute-1.amazonaws.com",
+            databaseName = "d87s2lf0vv7l32",
+            userName = "ppxiknjbrpshfp",
+            password = "dadde9e960e7acc54bf9b09a35ef98f4ec01a149e1560b4a8c4f6909271cc76c",
+            port = "5432"
+    )
+    public static Result check(String nip) {
+        try {
+            JSONObject object = new JSONObject();
+            String select = "SELECT * FROM m_user WHERE nip = ? limit 1";
+
+            PreparedStatement preparedStatement = Connection.getConnection().prepareStatement(select);
+            preparedStatement.setString(1, nip);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("id");
+                String nipResult = rs.getString("nip");
+                String name = rs.getString("name");
+                int step = rs.getInt("step");
+
+                object.put("status", 1);
+                object.put("id", id);
+                object.put("nip", nipResult);
+                object.put("name", name);
+                object.put("step", step);
+            } else {
+                object.put("status", 0);
+            }
+
+            return Body.echo(enums.Result.REQUEST_OK, object.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return status(401, e.getMessage());
+        }
+    }
+
+    @CheckDatabase(
+            database = Database.POSTGRESQL,
+            host = "ec2-23-23-173-30.compute-1.amazonaws.com",
+            databaseName = "d87s2lf0vv7l32",
+            userName = "ppxiknjbrpshfp",
+            password = "dadde9e960e7acc54bf9b09a35ef98f4ec01a149e1560b4a8c4f6909271cc76c",
+            port = "5432"
+    )
+    @BodyParser.Of(value = BodyParser.Json.class , maxLength = 1024 * 1024 * 1024)
+    public static Result updateStep() {
+        try {
+            JsonNode body = request().body().asJson();
+            String sql = "UPDATE m_user set step = ? where id = ?";
+
+            PreparedStatement preparedStatement = Connection.getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, body.path("step").asInt());
+            preparedStatement.setInt(2, body.path("id").asInt());
+
+            int count = preparedStatement.executeUpdate();
+            return Body.echo(enums.Result.REQUEST_OK, "Inserting Success : " + count);
         } catch (Exception e) {
             e.printStackTrace();
             return status(401, e.getMessage());
