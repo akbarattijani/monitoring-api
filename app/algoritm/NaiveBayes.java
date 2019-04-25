21a6cf7847a64cb52b563b2ad82dfba35612fe55
Akbar <akbar.attijani@gmail.com>
2019-04-25 11:38:35 +0700

    Closing all connection when finish

--- app/algoritm/NaiveBayes.java
+++ app/algoritm/NaiveBayes.java
@@ -2,6 +2,7 @@ package algoritm;
 
 import model.ClassificationModel;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -14,21 +15,21 @@ import java.util.Map;
 public class NaiveBayes {
     public void classification(List<ClassificationModel> samples, String[] data) {
 //        List<ClassificationModel> result = new ArrayList<>();
-        Map<Integer, Double> classStore = probCi(samples);
-        Map<Integer, ArrayList<Double>> attributeStore = probXCi(samples, data);
-        Map<Integer, Double> attributeProb = multiplyProb(attributeStore);
-        Map<Integer, Double> probability = probXCiMultiplyCi(classStore, attributeProb);
+        Map<Integer, BigDecimal> classStore = probCi(samples);
+        Map<Integer, ArrayList<BigDecimal>> attributeStore = probXCi(samples, data);
+        Map<Integer, BigDecimal> attributeProb = multiplyProb(attributeStore);
+        Map<Integer, BigDecimal> probability = probXCiMultiplyCi(classStore, attributeProb);
 
         System.out.println("------------------ Naive Bayes (PROB CLASS) ----------------------");
-        for (Map.Entry<Integer, Double> entry : classStore.entrySet()) {
+        for (Map.Entry<Integer, BigDecimal> entry : classStore.entrySet()) {
             System.out.println("ID : " + entry.getKey() + "\tProb : " + entry.getValue());
         }
         System.out.println("------------------------------------------------------------------\n");
 
         System.out.println("--------------- Naive Bayes (STORE ATTRIBUTE) --------------------");
-        for (Map.Entry<Integer, ArrayList<Double>> entry : attributeStore.entrySet()) {
+        for (Map.Entry<Integer, ArrayList<BigDecimal>> entry : attributeStore.entrySet()) {
             System.out.print("ID : " + entry.getKey() + "\tData : ");
-            for (Double val : entry.getValue()) {
+            for (BigDecimal val : entry.getValue()) {
                 System.out.print(val + "\t");
             }
 
@@ -37,13 +38,13 @@ public class NaiveBayes {
         System.out.println("------------------------------------------------------------------\n");
 
         System.out.println("---------------- Naive Bayes (PROB ATTRIBUTE) --------------------");
-        for (Map.Entry<Integer, Double> entry : attributeProb.entrySet()) {
+        for (Map.Entry<Integer, BigDecimal> entry : attributeProb.entrySet()) {
             System.out.println("ID : " + entry.getKey() + "\tProb : " + entry.getValue());
         }
         System.out.println("------------------------------------------------------------------\n");
 
         System.out.println("----------------- Naive Bayes (PROBABILITY) ----------------------");
-        for (Map.Entry<Integer, Double> entry : probability.entrySet()) {
+        for (Map.Entry<Integer, BigDecimal> entry : probability.entrySet()) {
             System.out.println("ID : " + entry.getKey() + "\tProb : " + entry.getValue());
         }
         System.out.println("------------------------------------------------------------------\n");
@@ -51,25 +52,25 @@ public class NaiveBayes {
 //        return result;
     }
 
-    private Map<Integer, Double> probXCiMultiplyCi(Map<Integer, Double> probClass, Map<Integer, Double> probAttribute) {
-        Map<Integer, Double> result = new LinkedHashMap<>();
+    private Map<Integer, BigDecimal> probXCiMultiplyCi(Map<Integer, BigDecimal> probClass, Map<Integer, BigDecimal> probAttribute) {
+        Map<Integer, BigDecimal> result = new LinkedHashMap<>();
 
-        for (Map.Entry<Integer, Double> entry : probClass.entrySet()) {
-            double count = probAttribute.get(entry.getKey()) / entry.getValue();
+        for (Map.Entry<Integer, BigDecimal> entry : probClass.entrySet()) {
+            BigDecimal count = probAttribute.get(entry.getKey()).divide(entry.getValue());
             result.put(entry.getKey(), count);
         }
 
         return result;
     }
 
-    private Map<Integer, Double> multiplyProb(Map<Integer, ArrayList<Double>> attribute) {
-        Map<Integer, Double> result = new LinkedHashMap<>();
+    private Map<Integer, BigDecimal> multiplyProb(Map<Integer, ArrayList<BigDecimal>> attribute) {
+        Map<Integer, BigDecimal> result = new LinkedHashMap<>();
 
-        for (Map.Entry<Integer, ArrayList<Double>> entry : attribute.entrySet()) {
-            double count = 0.0;
+        for (Map.Entry<Integer, ArrayList<BigDecimal>> entry : attribute.entrySet()) {
+            BigDecimal count = new BigDecimal(0.0);
 
-            for (Double value : entry.getValue()) {
-                count *= value;
+            for (BigDecimal value : entry.getValue()) {
+                count = count.multiply(value);
             }
 
             result.put(entry.getKey(), count);
@@ -78,8 +79,8 @@ public class NaiveBayes {
         return result;
     }
 
-    private Map<Integer, ArrayList<Double>> probXCi(List<ClassificationModel> samples, String[] data) {
-        Map<Integer, ArrayList<Double>> result = new LinkedHashMap<>();
+    private Map<Integer, ArrayList<BigDecimal>> probXCi(List<ClassificationModel> samples, String[] data) {
+        Map<Integer, ArrayList<BigDecimal>> result = new LinkedHashMap<>();
         Map<Integer, Integer> probClass = new LinkedHashMap<>();
 
         for (ClassificationModel model : samples) {
@@ -95,20 +96,20 @@ public class NaiveBayes {
             int index = 0;
 
             for (Map.Entry<Integer, Integer> entry : probClass.entrySet()) {
-                double count = 0.0;
+                BigDecimal count = new BigDecimal(0.0);
 
                 for(ClassificationModel model : samples) {
                     if (value.equals(model.getBiner()[index]) && entry.getKey() == model.getId()) {
-                        count++;
+                        count = count.add(new BigDecimal(1));
                     }
                 }
 
-                ArrayList<Double> newData = result.get(entry.getKey());
+                ArrayList<BigDecimal> newData = result.get(entry.getKey());
 
-                if (count == 0.0) {
-                    newData.add(1.0);
+                if (count.compareTo(new BigDecimal(0.0)) == 0) {
+                    newData.add(new BigDecimal(1.0));
                 } else {
-                    newData.add(count / entry.getValue());
+                    newData.add(count.divide(new BigDecimal(entry.getValue())));
                 }
 
                 result.put(entry.getKey(), newData);
@@ -119,19 +120,19 @@ public class NaiveBayes {
         return result;
     }
 
-    private Map<Integer, Double> probCi(List<ClassificationModel> samples) {
-        Map<Integer, Double> result = new LinkedHashMap<>();
+    private Map<Integer, BigDecimal> probCi(List<ClassificationModel> samples) {
+        Map<Integer, BigDecimal> result = new LinkedHashMap<>();
 
         for (ClassificationModel model : samples) {
             if (!result.containsKey(model.getId())) {
-                result.put(model.getId(), 1.0);
+                result.put(model.getId(), new BigDecimal(0.0));
             } else {
-                result.put(model.getId(), result.get(model.getId()) + 1);
+                result.put(model.getId(), result.get(model.getId()).add(new BigDecimal(1)));
             }
         }
 
-        for (Map.Entry<Integer, Double> entry : result.entrySet()) {
-            entry.setValue(entry.getValue() / samples.size());
+        for (Map.Entry<Integer, BigDecimal> entry : result.entrySet()) {
+            entry.setValue(entry.getValue().divide(new BigDecimal(samples.size())));
         }
 
         return result;
