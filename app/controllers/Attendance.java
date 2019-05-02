6a2c930dd9ca37bbcad28d8c3a6d0cb9c3116b6a
Akbar <akbar.attijani@gmail.com>
2019-05-02 16:40:53 +0700

    create attendance request

---
+++ app/controllers/Attendance.java
@@ -0,0 +1,106 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import database.Connection.CheckDatabase;
+import database.Connection.Connection;
+import enums.Database;
+import httpactions.ApiAuth;
+import play.mvc.BodyParser;
+import play.mvc.Result;
+import utils.Body;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static play.mvc.Controller.request;
+import static play.mvc.Results.status;
+
+/**
+ * @author AKBAR <akbar.attijani@gmail.com>
+ */
+
+@ApiAuth
+public class Attendance {
+    @CheckDatabase(
+            database = Database.POSTGRESQL,
+            host = "ec2-23-23-173-30.compute-1.amazonaws.com",
+            databaseName = "d87s2lf0vv7l32",
+            userName = "ppxiknjbrpshfp",
+            password = "dadde9e960e7acc54bf9b09a35ef98f4ec01a149e1560b4a8c4f6909271cc76c",
+            port = "5432"
+    )
+    @BodyParser.Of(value = BodyParser.Json.class , maxLength = 1024 * 1024 * 1024)
+    public static Result insert() {
+        try {
+            final JsonNode body = request().body().asJson();
+            final int id = body.path("id").asInt();
+            final Date startDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(body.path("start_date").asText());
+            final double longitude = body.path("longitude").asDouble();
+            final double latitude = body.path("latitude").asDouble();
+
+            String sql = "INSERT INTO t_absen (id_user, start_date) values (?,?)";
+            PreparedStatement preparedStatement = Connection.getConnection().prepareStatement(sql);
+
+            preparedStatement.setInt(1, id);
+            preparedStatement.setTimestamp(2, new Timestamp(startDate.getTime()));
+
+            int count = preparedStatement.executeUpdate();
+
+            if (count > 0) {
+                String select = "SELECT * FROM t_absen WHERE id_user = ? ORDER BY id DESC LIMIT 1";
+
+                PreparedStatement prepaired = Connection.getConnection().prepareStatement(select);
+                prepaired.setInt(1, id);
+
+                ResultSet rs = prepaired.executeQuery();
+                if (rs.next()) {
+                    sql = "INSERT INTO t_absen_detail (id_absen, longitude, latitude) values (?,?,?)";
+                    preparedStatement = Connection.getConnection().prepareStatement(sql);
+
+                    preparedStatement.setInt(1, rs.getInt("id"));
+                    preparedStatement.setDouble(2, longitude);
+                    preparedStatement.setDouble(3, latitude);
+
+                    int countDetail = preparedStatement.executeUpdate();
+                    if (countDetail > 0) {
+                        // Closing database connection
+                        preparedStatement.close();
+                        prepaired.close();
+                        rs.close();
+                        Connection.disconnect();
+
+                        return Body.echo(enums.Result.REQUEST_OK, Boolean.TRUE.toString());
+                    } else {
+                        sql = "DELETE FROM t_absen WHERE id = ?";
+                        preparedStatement = Connection.getConnection().prepareStatement(sql);
+                        preparedStatement.setInt(1, rs.getInt("id"));
+                        preparedStatement.executeUpdate();
+
+                        // Closing database connection
+                        preparedStatement.close();
+                        prepaired.close();
+                        rs.close();
+                        Connection.disconnect();
+
+                        return Body.echo(enums.Result.REQUEST_OK, Boolean.FALSE.toString());
+                    }
+                } else {
+                    return Body.echo(enums.Result.REQUEST_OK, Boolean.FALSE.toString());
+                }
+            } else {
+                // Closing database connection
+                preparedStatement.close();
+                Connection.disconnect();
+
+                return Body.echo(enums.Result.REQUEST_OK, Boolean.FALSE.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Connection.disconnect();
+            return status(401, e.getMessage());
+        }
+    }
+}
