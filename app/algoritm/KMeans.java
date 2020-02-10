b1294a5d8415337129a1ae4ce28bca47aa03bbdb
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-10 17:41:55 +0700

    Closing all connection when finish

--- app/algoritm/KMeans.java
+++ app/algoritm/KMeans.java
@@ -124,8 +124,8 @@ public class KMeans {
             for (int j = 0; j < data.length; j++) {
                 int value = (int) data[j][data[j].length - 1];
                 if (value == i) {
-                    System.out.println("j : " + j);
-                    updateCluster[i] += String.valueOf(j);
+                    updateCluster[i] = updateCluster[i] + String.valueOf(j);
+                    System.out.println("update : " + updateCluster[i]);
                 }
             }
 
